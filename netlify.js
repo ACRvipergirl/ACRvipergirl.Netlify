0ecd12daf2b6423c513e78982a816a3488a7624f
ACRvipergirl <jsawyer@proforma.com>
2023-02-28 11:03:05 -0500

    test

--- netlify.js
+++ netlify.js
@@ -1,14 +1,15 @@
 netlifyIdentity.on('init', () => {
     initUser = netlifyIdentity.currentUser();
 });
+
 netlifyIdentity.on('login', () => {
     if (initUser == null) {
         window.location.replace('home')
     }
     netlifyIdentity.close();
 });
+
 netlifyIdentity.on('logout', () => {
     netlifyIdentity.close();
-    window.location.replace('/')
-;
+    window.location.replace('/');
 });
